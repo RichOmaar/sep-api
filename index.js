5948b12fe1e28e9b0168fc07f841246db14d5b9b
Juan Omar Camacho <39716286+RichOmaar@users.noreply.github.com>
2023-05-02 23:30:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const sep = require('./routes/sep.js');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8889;
 
 app.use(bodyParser.json());
 
